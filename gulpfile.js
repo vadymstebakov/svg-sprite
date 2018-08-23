a577952d7e4aa8b7de67458cf6920be551573e06
Vadym <vadim.stebakov.94@gmail.com>
2018-08-23 19:05:29 +0300

    svg-sprites start

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  del = require('del'),
+  svgSprite = require('gulp-svg-sprites'),
+  svgmin = require('gulp-svgmin'),
+  cheerio = require('gulp-cheerio'),
+  replace = require('gulp-replace');
+
+gulp.task('svg-sprite', function() {
+  return gulp.src("app/img/svg/**/*.svg")
+    // minify svg
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    // remove all fill and style declarations in out shapes
+    .pipe(cheerio({
+      run: function($) {
+        $('[fill]').removeAttr('fill');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: {
+        xmlMode: true
+      }
+    }))
+    // cheerio plugin create unnecessary string '>', so replace it.
+    .pipe(replace('&gt;', '>'))
+    // build svg sprite
+    .pipe(svgSprite({
+      mode: "symbols",
+      preview: false,
+      selector: "icon-%f",
+      svg: {
+        symbols: 'symbol_sprite.html'
+      }
+    }))
+    .pipe(gulp.dest("app/img/icon/"));
+});
+
+gulp.task('scss', function() {
+  return gulp.src('app/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  });
+});
+
+gulp.task('clean', function() {
+  return del.sync('dist');
+});
+
+
+gulp.task('watch', ['browser-sync'], function() {
+  gulp.watch('app/sass/**/*.scss', ['scss']);
+  gulp.watch('app/*html', browserSync.reload);
+  gulp.watch('app/js/*js', browserSync.reload);
+});
